339f1aa345739eb442bded567a7fb389bb612609
seongraeNaver <seongrae1997@naver.com>
2024-11-19 10:34:34 +0900

    [level 0] Title: 순서쌍의 개수, Time: 0.18 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.py
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.py
@@ -1,9 +1,10 @@
 def solution(n):
     answer = 0
-    for i in range(1, n+1):
-        if n % i == 0:
+    for i in range(1, int(n**(1/2))+1):
+        if i*i == n:
             answer += 1
+        elif n % i == 0:
+            answer += 2
         else:
             continue
-            
     return answer
