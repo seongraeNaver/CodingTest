c1d94d7e6a5edc669431fcf843ee12c8a9c2255b
ksr-dev <seongrae1997@naver.com>
2024-12-19 16:25:59 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.03 ms, Memory: 9.99 MB -BaekjoonHub

--- 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.py
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.py
@@ -1,15 +1,10 @@
 def solution(rank, attendance):
-    eligible = []
-    for i in range(len(rank)):
-        if attendance[i]:
-            eligible.append((rank[i], i))
-            
-    eligible.sort()
-    
-    top3 = []
-    for j in range(3):
-        top3.append(eligible[j][1])
-        
-    result = 10000*top3[0] + 100*top3[1] + top3[2]
-    
-    return result
+    selected = []
+    for i, attend in enumerate(attendance):
+        if attend:
+            selected.append((rank[i], i))
+
+    selected.sort()
+    a, b, c = selected[:3]
+
+    return 10000 * a[1] + 100 * b[1] + c[1]
